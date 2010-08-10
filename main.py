58828871fe3140213b1fb269f5f08a97ba0648fd
Matt Mayer <matt@matthewmayer.co.uk>
2010-08-10 12:14:27 +0800

    change var name

--- main.py
+++ main.py
@@ -10,22 +10,29 @@ class Hook(webapp.RequestHandler):
 
     def post(self):
         
-        
+        #get the credentials for the nazaztag 
         u = self.request.get("id")
         p = self.request.get("secret")
+        
+        #get the JSON payload. this is stored in the POST variable "payload"
         jsonstr = self.request.get("payload")
+        
+        #parse it
         obj = json.loads(jsonstr)
+        
+        #read out some interesting data
         reponame = obj['repository']['name']
         author = obj['commits'][0]['author']['name']
         msg = obj['commits'][0]['message']
         numcommits = len(obj['commits'])
-        str = "Git alert! Git alert! %s just pushed %d commits to %s" %(author,numcommits,reponame)
-        self.response.out.write(str)
         
+        #construct the string we want to send to the rabbit
+        txt = "Git alert! Git alert! %s just pushed %d commits to %s" %(author,numcommits,reponame)
+        self.response.out.write(txt)
+        
+        #construct the nabaztag object and send the text
         myNabaztag = Nabaztag(u,p)
-       
-        
-        myNabaztag.say(str,voice='US-Liberty')
+        myNabaztag.say(txt,voice='US-Liberty')
 
 
 application = webapp.WSGIApplication([
